fdf5620467df3b1f3666cb0814bfb25fc0be5af0
maximart <maxime.martinez96@hotmail.fr>
2024-12-04 10:55:19 +0100

    Fix

--- server.c
+++ server.c
@@ -14,9 +14,9 @@
 
 
 
-void	handle_signal(int signal)
+void	get_signal(int signal)
 {
-	static unsigned char	buffer[BUFFER_SIZE + 1];
+	static unsigned char	buffer[BUFFER_SIZE];
 	static unsigned char	current_char = 0;
 	static int				bit_index = 0;
 	static int				char_index = 0;
@@ -47,15 +47,14 @@ int	main(int argc, char **argv)
 	pid_t pid;
 
 	(void)argv;
-	act.sa_handler = handle_signal;
+	act.sa_handler = get_signal;
 	if (argc != 1)
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
-
 	pid = getpid();
-	printf("Server PID: %d\n", pid);
+	printf("PID: %d\n", pid);
 	while (argc == 1)
 	{
 		sigaction(SIGUSR1, &act, NULL);
